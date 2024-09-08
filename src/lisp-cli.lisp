f3bffa4122199217369511f3463037eca742ad63
haruki7049 <tontonkirikiri@gmail.com>
2024-09-08 23:45:47 +0900

    style: Format by lisp-format

--- src/lisp-cli.lisp
+++ src/lisp-cli.lisp
@@ -8,14 +8,14 @@
 (defun greet/options ()
   "Returns the options for the 'greet' command"
   (list
-    (clingon:make-option
-      :string
-      :description "Person to greet"
-      :short-name #\u
-      :long-name "user"
-      :initial-value "haruki7049"
-      :env-vars '("USER")
-      :key :user)))
+   (clingon:make-option
+    :string
+    :description "Person to greet"
+    :short-name #\u
+    :long-name "user"
+    :initial-value "haruki7049"
+    :env-vars '("USER")
+    :key :user)))
 
 (defun greet/handler (cmd)
   "Handler for the 'greet' command"
@@ -25,15 +25,15 @@
 (defun greet/command ()
   "A command to greet someone"
   (clingon:make-command
-    :name "greet"
-    :description "greets people"
-    :version "0.1.0"
-    :authors '("haruki7049 <tontonkirikiri@gmail.com>")
-    :license "MIT"
-    :options (greet/options)
-    :handler #'greet/handler))
+   :name "greet"
+   :description "greets people"
+   :version "0.1.0"
+   :authors '("haruki7049 <tontonkirikiri@gmail.com>")
+   :license "MIT"
+   :options (greet/options)
+   :handler #'greet/handler))
 
 (defun main ()
   "CLI application's entrypoint"
   (let ((app (greet/command)))
-        (clingon:run app)))
+    (clingon:run app)))
